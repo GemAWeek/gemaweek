ac7749de35849b66b8a457a45e33257d53a4495d
Evan Machnic <emachnic@broadmac.net>
2011-07-04 15:06:22 -0400

    Version 0.0.3

--- lib/gemaweek.rb
+++ lib/gemaweek.rb
@@ -2,5 +2,8 @@ require "gemaweek/version"
 require 'api_smith'
 
 module Gemaweek
+  class Client
+    include APISmith::Client
+    base_uri 'http://localhost:3000'
+  end
 end
-require "gemaweek/client"
